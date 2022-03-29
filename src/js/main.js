633a39728c696410a1247a478b567907243e54ea
lonelybuddy <henryzheng92@gmail.com>
2022-03-28 21:16:52 -0500

    added services poppu modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,25 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const header = document.querySelector('#header');
 
-window.addEventListener('scroll', scrollHeader);
-
 function scrollHeader() {
   header.classList.toggle('scroll-header', window.scrollY >= 50);
 }
+
+window.addEventListener('scroll', scrollHeader);
+
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal');
+const modalButtons = document.querySelectorAll('.services__button');
+const modalCloseButtons = document.querySelectorAll('.services__modal--close');
+
+function modal(modalClick) {
+  modalViews[modalClick].classList.toggle('active-modal');
+}
+
+modalButtons.forEach((mb, i) => {
+  mb.addEventListener('click', () => modal(i));
+});
+
+modalCloseButtons.forEach((mcb, i) => {
+  mcb.addEventListener('click', () => modal(i));
+});
