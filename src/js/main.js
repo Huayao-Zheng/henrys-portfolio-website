2c39a73d2dc7bd9228820355dad8d58e2ba12a30
lonelybuddy <henryzheng92@gmail.com>
2022-03-29 18:39:24 -0500

    added slider for testimonial using Swiper.js

--- src/js/main.js
+++ src/js/main.js
@@ -43,3 +43,23 @@ function activeWork() {
 }
 
 linkWork.forEach((lw) => lw.addEventListener('click', activeWork));
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
+});
